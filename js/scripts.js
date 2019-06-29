b52b907746315a829210a83410d41cdb46c32e8c
Rusty Abbott <rusty@hutchinson-island.com>
2019-06-29 13:31:22 -0400

    Draw the hour notches on clock

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+let secondHandLength = canvas.width / 2 - 16;
 
 // Paint canvas every second
 setInterval(paintCanvas, 1000);
@@ -8,10 +9,11 @@ function paintCanvas() {
   // Clear the canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Draw circles
+  // Draw circles & notches
   outerCircleOut();
   outerCircleIn();
   centerCircle();
+  hourNotches();
 
   function outerCircleOut() {
     ctx.beginPath();
@@ -33,5 +35,20 @@ function paintCanvas() {
     ctx.arc(canvas.width / 2, canvas.height / 2, 4, 0, Math.PI * 2);
     ctx.strokeStyle = '#fff';
     ctx.stroke();
+  }
+
+  // Draw the hour notches
+  function hourNotches() {
+    for (let i = 0; i < 12; i++) {
+      let angle = (i - 3) * (Math.PI * 2) / 12;
+      ctx.beginPath();
+      let x1 = (canvas.width / 2) + Math.cos(angle) * (secondHandLength);
+      let y1 = (canvas.height / 2) + Math.sin(angle) * (secondHandLength);
+      let x2 = (canvas.width / 2) + Math.cos(angle) * (secondHandLength - (secondHandLength / 8));
+      let y2 = (canvas.height / 2) + Math.sin(angle) * (secondHandLength - (secondHandLength / 8));
+      ctx.moveTo(x1, y1);
+      ctx.lineTo(x2, y2);
+      ctx.stroke();
+    }
   }
 }
